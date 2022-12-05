6a7917697c6b5dd9d974b138af03f409142caa02
feffe <feffe@feffe.dev>
2022-12-05 23:51:18 +0100

    Add base project thing

---
+++ xx/src/main.rs
@@ -0,0 +1,9 @@
+use nom::bytes::complete::tag;
+use nom::character::complete::u32;
+use nom::character::complete::{char, digit1};
+use nom::combinator::{map, map_res};
+use nom::sequence::{pair, preceded, separated_pair};
+use nom::IResult;
+
+use std::collections::VecDeque;
+fn main() {}
